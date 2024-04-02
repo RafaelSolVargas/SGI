362828a793347ee3ac3ab40f5bed993ba4b2fd03
thiagobew <thiagoaugustobewiahn@gmail.com>
2024-04-02 09:33:18 -0300

    Line clipping interface

---
+++ src/Domain/Management/Clipping.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from src.Domain.Management.Window import Window
+from src.Domain.Shapes.Line import Line
+from src.Domain.Shapes.SGIObject import SGIObject
+
+class LineClippingStrategy(ABC):
+    def __init__(self) -> None:
+        pass
+            
+    @abstractmethod
+    def __clip_line(self, line: Line, window: Window) -> Line | None:
+        pass
+    
+    # TODO: define if objs should be a list of SGIObject or Line (conversion here or in the caller?)
+    def clip(self, window: Window, objs: list[Line]) -> list[Line]:
+        clipped_objs = []
+        
+        for obj in objs:
+            temp = self.__clip_line(obj, window)
+            
+            if temp is not None:
+                clipped_objs.append(temp)
+        
+        return clipped_objs
